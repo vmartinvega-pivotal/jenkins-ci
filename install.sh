d15fe8e1936b86e58d92dd2878b73a91044d29dc
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2020-05-12 09:03:05 +0200

    Added template for jenkins configuration as code

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ MINIKUBE_CLIENT_CERTIFICATE=$(kubectl config view -o jsonpath='{.users[?(@.name 
 MINIKUBE_CLIENT_KEY=$(kubectl config view -o jsonpath='{.users[?(@.name == "minikube")].user.client-key}')
 CA_CERTIFICATE=$(kubectl config view -o jsonpath='{.clusters[?(@.name == "minikube")].cluster.certificate-authority}')
 KUBERNETES_URL=$(kubectl config view -o jsonpath='{.clusters[?(@.name == "minikube")].cluster.server}')
+MINIKUBE_IP=$(minikube ip)
 
 # Update the certs path in the kubeconfig file with embed certificates
 # this is needed to configure the kubernetes plugin for jenkins
@@ -66,13 +67,13 @@ if [[ $INSTALL_JENKINS = "true" ]]
 then
     minikube ssh "cd $MINIKUBE_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins && docker build -t c3alm-sgt/jenkins ."
     kubectl create -f $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/jenkins-deployment.yaml
-    #kubectl create serviceaccount jenkins
-    #kubectl create -f $HOST_PROJECTS_FOLDER/jenkins-k8s-configuration/pre/sgt-secrets.yaml
-    #kubectl create -f $HOST_PROJECTS_FOLDER/jenkins-k8s-configuration/pre/sgt-config-map.yaml
-    #MINIKUBE_IP=$(minikube ip)
-    #sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/jenkins_yaml.yml > $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/output.file
-    #kubectl create -f $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/output.file
-    #rm $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/output.file
+    ./kubernetes/wait-until-pods-ready.sh 60 5
+    INTERNAL_IP=$(kubectl get pod -o jsonpath='{.items[0].status.podIP}')
+    KUBECONFIG_FILE_BYTES=$(cat ${HOME}/.kube/config | base64 --wrap=0)
+    sed "s/JENKINS_URL/http:\/\/$INTERNAL_IP:8080/g" $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/jenkins-conf-template.yaml > $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/output.file
+    sed "s/JENKINS_TUNNEL/http:\/\/$INTERNAL_IP:50000/g" $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/output.file > $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/output1.file
+    sed "s/KUBERNETES_URL/https:\/\/$MINIKUBE_IP:8443/g" $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/output1.file > $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/output2.file
+    sed "s/KUBECONFIG_FILE_BYTES/$KUBECONFIG_FILE_BYTES/g" $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/output2.file > $HOST_PROJECTS_FOLDER/jenkins-pipeline-k8s-test/jenkins/jenkins-conf.yaml
 fi
 
 if [[ $INSTALL_AGENTS = "true" ]]
@@ -83,26 +84,3 @@ then
     MAVEN_JNLP_AGENT_FOLDER=maven-jnlp-agent
     minikube ssh "cd $MINIKUBE_PROJECTS_FOLDER/$MAVEN_JNLP_AGENT_FOLDER/ && docker build -t c3alm-sgt/maven-jnlp-agent ."
 fi
-
-#NODE_PORT=$(kubectl get services jenkins --namespace jenkins -o jsonpath='{.spec.ports[0].nodePort}')
-#MINIKUBE_IP=$(minikube ip)
-
-#echo ""
-#echo "***************************************"
-#echo ""
-#echo "Jenkins Url to access: http://$MINIKUBE_IP:$NODE_PORT"
-#echo ""
-#INTERNAL_IP=$(kubectl get pod --namespace jenkins -o jsonpath='{.items[0].status.podIP}')
-
-#echo "Jenkins Url to configure Kubernetes Plugin: http://$INTERNAL_IP:8080"
-#echo ""
-#echo "Junkins Tunnel to configure Kubernetes Plugion: http://$INTERNAL_IP:50000"
-#echo ""
-
-#cp ${HOME}/.kube/config ./config
-#PWD=$(pwd)
-#echo "Configure a secret file for kubernetes config file located at: $PWD/config"
-#echo ""
-#echo "Configure Kubernetes Url: $KUBERNETES_URL"
-#echo ""
-#echo "***************************************"
